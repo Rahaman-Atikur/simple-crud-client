95a937685b1c09a066984e7ca8822ae9a7729e29
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-11-08 00:34:39 +0000

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Users from './components/Users'
+function App() {
+  return (
+    <>
+     <h2>The user name has not  come yet </h2>
+     <Users></Users>
+    </>
+  )
+}
+export default App
---
+++ src/components/Users.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Users = () => {
+    const handleUser =e=>{
+        e.preventDefault();
+
+    }
+    return (
+        <div>
+            <div>
+                {/* Add User */}
+                <form>
+                    <input name='nameField' type="text" />
+                    <br />
+                    <input name='emailField' type="text" />
+                    <br />
+                    <input type="submit" value="addUser" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Users;
