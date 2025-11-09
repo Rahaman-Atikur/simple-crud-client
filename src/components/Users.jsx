b3821297f411b2c7a5d0477f445f37233f34982c
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-11-09 00:07:57 +0000

    Every Input Field is done

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-
 const Users = () => {
-    const handleUser =e=>{
+    const handleUser = e => {
         e.preventDefault();
-
+        const name = e.target.nameField.value;
+        const email = e.target.emailField.value;
+        console.log(name, email);
     }
     return (
         <div>
             <div>
                 {/* Add User */}
+                <h1>Simple Crud Applications</h1>
                 <form onSubmit={handleUser}>
                     <input name='nameField' type="text" />
                     <br />
                     <input name='emailField' type="text" />
                     <br />
-                    <input type="submit" value="addUser" />
+                    <input type="submit" value="AddUser" />
                 </form>
             </div>
         </div>
     );
 };
-
 export default Users;
